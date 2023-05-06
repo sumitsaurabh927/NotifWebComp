6cd870d65fe01744bfa08c6b416b6e8f8f51f862
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2023-05-06 22:42:00 +0530

    testing FE

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#form')
+
+
+// extracting send button
+const btn = document.querySelector('#btn');
+const input = document.querySelector('.input')
+
+// btn.onclick = () => console.log('clicked');
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await fetch('https://notificationsgeneratorbackend.onrender.com/home', {
+        method: 'POST',
+        body: JSON.stringify({
+            description: input.value,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    
+})
+
+// extracting input value on button click
+
+// here you can attach any callbacks, interact with the web component API
+let nc = document.getElementsByTagName('notification-center-component')[0];
+nc.onLoad = () => console.log('hello world!');
